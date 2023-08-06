8496b1a9ebb96d2b4d1202a89433ccb1a68b6160
Joerg Braeutigam <braeutigam.joerg@gmail.com>
2023-08-06 18:22:57 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Logo from "./components/Logo";
 import ShowEvents from "./components/Showevents";
 import Adressinput from "./components/Adressinput";
